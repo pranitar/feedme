bd7eaf898633725c4f591436b38d5b4415075725
unknown <pranita25@gmail.com>
2014-09-18 18:01:09 -0400

    YAY WE ROCK

--- public/js/script.js
+++ public/js/script.js
@@ -27,7 +27,7 @@ $(function() {
         cache: false,
         url: "https://api.instagram.com/v1/media/popular?client_id=17b605e326494ebf958596b21441d8df",
         success: function(data) {
-            for (var i = 0; i < 10; i++) {
+            for (var i = 0; i < 50; i++) {
                 $("#pics").append("<a target='_blank' href='" + data.data[i].link + "'><img src='" + data.data[i].images.low_resolution.url + "'></img></a>");
             }
         }
