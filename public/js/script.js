aefa5a09d3185f17ea97bc2d805a147d33bf527c
unknown <pranita25@gmail.com>
2014-09-18 18:03:34 -0400

    trial

--- public/js/script.js
+++ public/js/script.js
@@ -25,9 +25,9 @@ $(function() {
         type: "GET",
         dataType: "jsonp",
         cache: false,
-        url: "https://api.instagram.com/v1/media/popular?client_id=17b605e326494ebf958596b21441d8df",
+        url: "https://api.instagram.com/v1/tags/food/media/recent?client_id=17b605e326494ebf958596b21441d8df",
         success: function(data) {
-            for (var i = 0; i < 50; i++) {
+            for (var i = 0; i < 15; i++) {
                 $("#pics").append("<a target='_blank' href='" + data.data[i].link + "'><img src='" + data.data[i].images.low_resolution.url + "'></img></a>");
             }
         }
