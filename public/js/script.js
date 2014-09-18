3f7f7cae6a194da374bf8c602ba76e826fe0a766
unknown <pranita25@gmail.com>
2014-09-18 17:59:12 -0400

    TRIAL 1 LETSS EEEE HOW THIS GOESSSS

--- public/js/script.js
+++ public/js/script.js
@@ -1,37 +1,38 @@
-$(function() {    // do once original document loaded and ready
-        $('form[name="eg1"] input').click(function() {
-        	$.ajax({
-            crossDomain: true,
-            type:"GET",
-            contentType: "application/json; charset=utf-8",
-            async:false,
-            url: "https://api.instagram.com/v1/tags/food/media/recent?client_id=17b605e326494ebf958596b21441d8df&callback=display",
-            data: {projectID:1},
-            dataType: "jsonp",                
-            jsonpCallback: 'fnsuccesscallback'
-        });
-        	    
-        	    // $.getJSON("dance.json", function(responseObject, diditwork) {
-                //         console.log(diditwork);
-                //         var displayText = 
-                //                 "There are " 
-                //                 + responseObject.indianDances.length 
-                //                 + " Indian dances:<ol>";
-                //         for (var i = 0; i<responseObject.indianDances.length; i++) {
-                //                 var dance = responseObject.indianDances[i];
-                //                 displayText += "<li>"
-                //                                         +dance.danceName + " " 
-                //                                         + dance.region + "<\/li>";
-                //                 }
-                //         displayText += "<\/ul>";
-                // $("#jqueryresponse").html(displayText);
-                // } );  // getJSON
-        } );  // click
-  } ); // onReady
+// $(function() {    // do once original document loaded and ready
+//         $('form[name="eg1"] input').click(function() {
+//         	$.ajax({
+//         	dataType: "jsonp", 
+//             crossDomain: true,
+//             type:"GET",
+//             contentType: "application/json; charset=utf-8",
+//             async:false,
+//             cache: false,
+//             url: "https://api.instagram.com/v1/tags/food/media/recent?client_id=17b605e326494ebf958596b21441d8df",
+//             // data: {projectID:1},               
+//             // jsonpCallback: 'fnsuccesscallback'
+//             success: function(data){
+//             	for (var i = 0; i < 10; i++) {
+//                 $("#pics").append("<a target='_blank' href='" + data.data[i].link + "'><img src='" + data.data[i].images.low_resolution.url + "'></img></a>");
+//             }
+//             }
+//         	});
+//         } );  // click
+//   } ); // onReady
 
-function display(tag) {
+$(function() {
 
-}
+    $.ajax({
+        type: "GET",
+        dataType: "jsonp",
+        cache: false,
+        url: "https://api.instagram.com/v1/media/popular?client_id=17b605e326494ebf958596b21441d8df",
+        success: function(data) {
+            for (var i = 0; i < 10; i++) {
+                $("#pics").append("<a target='_blank' href='" + data.data[i].link + "'><img src='" + data.data[i].images.low_resolution.url + "'></img></a>");
+            }
+        }
+    });
+});
 
 
 
